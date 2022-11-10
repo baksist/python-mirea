2a547b921ce37c098ea9edc450b0ae5dcc0a3926
matt <matvey.serdukov@gmail.com>
2022-11-10 19:44:15 +0300

    logic mod + fonts

--- calculator.py
+++ calculator.py
@@ -9,36 +9,41 @@ def get_center_offset(width, height):
     return left, top
 
 def init_form():
+
+    def check_second_arg():
+        return len(result_label["text"].split('+')) == 2
       
     def button_num1_click():
+        if (check_second_arg()):
+            button_eq.configure(state = "normal")
         result_label["text"] += "1"
     
     def button_num2_click():
+        if (check_second_arg()):
+            button_eq.configure(state = "normal")
         result_label["text"] += "2"
     
     def button_plus_click():
         result_label["text"] += "+"
-        button_plus.configure(state="disabled")
+        button_plus.configure(state = "disabled")
     
     def button_eq_click():
         args = result_label["text"].split('+')
-        if len(args) != 2:
-            return
         result_label["text"] += f"={int(args[0])+int(args[1])}"
-        button_num1.configure(state="disabled")
-        button_num2.configure(state="disabled")
-        button_eq.configure(state="disabled")
+        button_num1.configure(state = "disabled")
+        button_num2.configure(state = "disabled")
+        button_eq.configure(state = "disabled")
    
     def button_clear_click():
         result_label["text"] = ""
-        button_num1.configure(state="normal")
-        button_num2.configure(state="normal")
-        button_plus.configure(state="normal")
-        button_eq.configure(state="normal")
+        button_num1.configure(state = "normal")
+        button_num2.configure(state = "normal")
+        button_plus.configure(state = "normal")
+        button_eq.configure(state = "disabled")
 
     form = Tk()
 
-    width, height = 800, 600
+    width, height = 640, 480
     left, top = get_center_offset(width, height)
     form_dimensions = f"{width}x{height}+{left}+{top}"
     app_title = "Simple Calculator"
@@ -49,16 +54,17 @@ def init_form():
     button_num1 = Button(text = "1", command = button_num1_click)
     button_num2 = Button(text = "2", command = button_num2_click)
     button_plus = Button(text = "+", command = button_plus_click)
-    button_eq = Button(text = "=", command = button_eq_click)
+    button_eq = Button(text = "=", command = button_eq_click, state = "disabled")
     button_clear = Button(text = "Clear", command = button_clear_click)
     buttons = [button_num1, button_num2, button_plus, button_eq, button_clear]
 
-    result_label = Label(text = "")
-    result_label.place(x = width // 2, y = height // 3)
+    result_label = Label(text = "", font = ("Arial", 18))
+    result_label.place(x = width // 6, y = height // 3)
 
     but_x, but_y = width // 6, 2 * (height // 3)
     
     for button in buttons:
+        button.configure(font = ("Arial", 18))
         button.place(x = but_x, y = but_y)
         but_x += width // 8
 
